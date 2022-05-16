dce4ab2ac407d517e7feb0c3566294b053b52b05
sapir.shapira <sapir.shapira@cs.huji.ac.il>
2022-05-16 14:51:15 +0300

    merge tail new fields from preprocess analysis

--- fish_preprocessed_data.py
+++ fish_preprocessed_data.py
@@ -30,6 +30,7 @@ It wraps a mat file with class methods for loading/dumping data, therefore it sh
 
 def to_valid_str(data):
     """savemat and loadmat doesn't recognize well empty char string (loaded as float array). Therefore empty str is " ".
+
     :param data:
     :return: valid string
     """
@@ -65,6 +66,8 @@ class Points:
 
     @classmethod
     def import_from_struct(cls, data):
+        if len(data) == 0:  # empty
+            return cls([np.nan], [np.nan])
         return cls(data['x'], data['y'])
 
     @staticmethod
@@ -79,7 +82,7 @@ class Points:
         :param array_2d:
         :return:
         """
-            
+
         if cls.is_valid_points(array_2d):
             if cast_to_float:
                 return cls(array_2d[:, 0].astype(float), array_2d[:, 1].astype(float))
@@ -182,8 +185,6 @@ class Head:
                 raise TypeError("{0} values are not is {1}-{2} instead of 0-360".format(name_,
                                                                                         np.min(curr_eyes_list),
                                                                                         np.max(curr_eyes_list)))
-            # if curr_eyes_list.shape == (2,):
-            #     print("event has no frames")
 
     def export_to_struct(self):
         """
@@ -200,6 +201,7 @@ class Head:
     def import_from_struct(cls, data):
         """Load matlab data as Head class instance.
         Note: directions_in_deg is calculated from points, and not loaded.
+
         :param data: input data - dictionary matching export_to_struct output (mirror of this function)
         :return: Head class, filled with data.
         """
@@ -211,38 +213,50 @@ class Head:
 
 
 class Tail:
-    def __init__(self, tail_tip_points: Points, is_bout_frame_list: List[bool], tail_path_list: List[Points],
-                 velocity_norms: List[float]):
+    def __init__(self, tail_tip_points: Points, is_bout_frame_list: List[bool], bout_start_frames: List,
+                 bout_end_frames: List, tail_path_list: List[Points], interpolated_tail_path: List,
+                 swimbladder_points_list: Points, tip_to_swimbladder_distance: List, velocity_norms: List[float]):
         """todo explain each parameter
 
         :param tail_tip_points: list of points referring to tips of the tail at different frames
         :param tail_path_list: list of arrays containing all of the midline (tail points) from tail tip to swimbladder, each entry refers to a different frame
-        :param is_bout_frame_list:
+        :param swimbladder_points_list: list of points referring to swimbladder points at different frames
+        :param tip_to_swimbladder_distance: list of distances between tip of the tail to the swimbladder point at different frames
+        :param is_bout_frame_list: List of boolean values indicating if the frame is during a bout or not
+        :param bout_start_frames: List of bout start frames
+        :param bout_end_frames: List of bout end frames
         :param velocity_norms:
         """
         validate_type(tail_tip_points, Points)
+        validate_type(swimbladder_points_list, Points)
 
         # todo this is an error. The usage of properties protects the class attributes
         self.tail_tip_point_list = tail_tip_points
-        self.tail_path_list = tail_path_list
+        self.swimbladder_points_list = swimbladder_points_list
+        self.tip_to_swimbladder_distance = tip_to_swimbladder_distance
+        self.tail_path_list = get_validated_list(tail_path_list, inner_type=Points)
+        self.interpolated_tail_path = get_validated_list(interpolated_tail_path,inner_type=float)
         self.is_bout_frame_list = np.array(get_validated_list(is_bout_frame_list, inner_type=bool))
+        self.bout_start_frames = np.array(get_validated_list(bout_start_frames, inner_type=(int, np.int64)))
+        self.bout_end_frames = np.array(get_validated_list(bout_end_frames, inner_type=(int, np.int64)))
         self.velocity_norms = np.array(get_validated_list(velocity_norms, inner_type=float))
 
     def export_to_struct(self):
-        tail_path_list_exported = []
-        for tail_path in self.tail_path_list:
-            if type(tail_path) is Points:  # todo this is a bug
-                tail_path = tail_path.export_to_struct()
+        tail_path_list_exported = [tail_path.export_to_struct() for tail_path in self.tail_path_list]
 
-            tail_path_list_exported.append(tail_path)
         return {'tail_tip_point_list': self.tail_tip_point_list.export_to_struct(),
                 'tail_path_list': tail_path_list_exported,
+                'interpolated_tail_path': self.interpolated_tail_path,
                 'is_bout_frame_list': self.is_bout_frame_list,
+                'bout_start_frames': self.bout_start_frames,
+                'bout_end_frames': self.bout_end_frames,
+                'swimbladder_points_list': self.swimbladder_points_list.export_to_struct(),
+                'tip_to_swimbladder_distance': self.tip_to_swimbladder_distance,
                 'velocity_norms': self.velocity_norms}
 
     @classmethod
     def import_from_struct(cls, data):
-        if 'tail_path_list' not in data.keys() or len(data['tail_path_list']) == 0:
+        if 'tail_path_list' not in data.keys() or len(data['tail_path_list']) == 0:  # todo patch due to bug above. should be removed
             tail_path_list_as_points = []
         else:
             if isinstance(data['tail_path_list'][0], dict):  # todo patch due to bug above. should be removed
@@ -251,20 +265,19 @@ class Tail:
                 tail_path_list_as_points = [Points.from_array(np.array(tail_path), cast_to_float=True) for tail_path in
                                             data['tail_path_list']]
         return cls(tail_tip_points=Points.import_from_struct(data['tail_tip_point_list']),
-                   is_bout_frame_list=data['is_bout_frame_list'],
+                   is_bout_frame_list=data.get('is_bout_frame_list', []),
+                   bout_start_frames=data.get('bout_start_frames', []),
+                   bout_end_frames=data.get('bout_end_frames', []),
                    tail_path_list=tail_path_list_as_points,
-                   velocity_norms=data.get('velocity_norms', data.get('velocity_norms_convolution', [])))
+                   interpolated_tail_path=data.get('interpolated_tail_path', []),
+                   swimbladder_points_list=Points.import_from_struct(data.get('swimbladder_points_list', [])),
+                   tip_to_swimbladder_distance=data.get('tip_to_swimbladder_distance', np.nan),
+                   velocity_norms=data.get('velocity_norms', []))
 
 
 class Paramecium:
     @property
     def center_points(self):
-        """
-        n = rows represents number of frames in this event.
-        m = columns represents number of paramecium in this event.
-
-        :return: nd.array center: nXmX2 where 2 = the centroid of each paramecia at each frames is (x,y).
-        """
         return self._center_points
 
     @property
@@ -341,9 +354,12 @@ class Paramecium:
 
     @classmethod
     def import_from_struct(cls, data):  # match ctor
-        return cls(center=data['center'], area=data['area'], status=data['status'], color=data['color'],
-                   ellipse_majors=data['ellipse_majors'], ellipse_minors=data['ellipse_minors'],
-                   ellipse_dirs=data['ellipse_dirs_in_deg'], bounding_boxes=data['bounding_boxes'])
+        return cls(center=data['center'], area=data.get('area', []), status=data['status'],
+                   color=data.get('color',[]),
+                   ellipse_majors=data.get('ellipse_majors', []),
+                   ellipse_minors=data.get('ellipse_minors',[]),
+                   ellipse_dirs=data.get('ellipse_dirs_in_deg',[]),
+                   bounding_boxes=data.get('bounding_boxes',[]))
 
 
 class Event:
@@ -353,6 +369,8 @@ class Event:
     _paramecium: Paramecium
     _fish_tracking_status_list: np.ndarray
     _tail_tip_status_list: np.ndarray
+    _swimbladder_points_status_list: np.ndarray
+    _fish_area_in_pixels: np.ndarray
     _event_name: str
     _event_id: int
 
@@ -376,16 +394,24 @@ class Event:
         return self._paramecium
 
     @property
+    def fish_area_in_pixels(self):
+        return self._fish_area_in_pixels
+
+    @property
     def fish_tracking_status_list(self):
         return self._fish_tracking_status_list
 
     @property
-    def  is_head_prediction_list(self):
+    def is_head_prediction_list(self):
         return self._is_head_prediction_list
 
     @property
     def tail_tip_status_list(self):
         return self._tail_tip_status_list
+
+    @property
+    def swimbladder_points_status_list(self):
+        return self._swimbladder_points_status_list
 
     @property
     def event_id(self):
@@ -413,8 +439,15 @@ class Event:
             return sum(self.tail_tip_status_list) / len(self.tail_tip_status_list)
         return 0
 
+    @property
+    def swimbladder_found_percentage(self):
+        if len(self.swimbladder_points_status_list) > 0:
+            return sum(self.swimbladder_points_status_list) / len(self.swimbladder_points_status_list)
+        return 0
+
     def __init__(self, event_name: str, event_id: int, head: Head, tail: Tail, fish_tracking_status_list: List[bool],
-                 is_head_prediction_list:List[bool], tail_tip_status_list: List[bool], paramecium: Paramecium):
+                 is_head_prediction_list: List[bool], tail_tip_status_list: List[bool],  swimbladder_points_status_list: List[bool], paramecium: Paramecium,
+                 fish_area_in_pixels: List[float]):
 
         validate_type(paramecium, Paramecium)
         validate_type(head, Head)
@@ -428,8 +461,10 @@ class Event:
         self._fish_tracking_status_list = get_validated_list(fish_tracking_status_list, inner_type=(bool, np.bool_))
         self._is_head_prediction_list = get_validated_list(is_head_prediction_list, inner_type=(bool, np.bool_))
         self._tail_tip_status_list = get_validated_list(tail_tip_status_list, inner_type=(bool, np.bool_))
+        self._swimbladder_points_status_list = get_validated_list(swimbladder_points_status_list, inner_type=(bool, np.bool_))
         self._event_id = event_id
         self._event_name = to_valid_str(event_name)
+        self._fish_area_in_pixels = get_validated_list(fish_area_in_pixels, inner_type=float)
 
     def set_metadata(self, is_complex_hunt: bool, outcome: int, outcome_str: str, comments: str, event_frame_ind: int):
         """Set metadata parameters from an external script (look at pipeline scripts)
@@ -461,9 +496,12 @@ class Event:
                 'fish_tracking_status_list': self.fish_tracking_status_list,
                 'fish_tracking_found_percentage': self.fish_tracking_found_percentage,
                 'is_head_prediction_list': self.is_head_prediction_list,
+                'fish_area_in_pixels': self.fish_area_in_pixels,
                 'fish_prediction_percentage': self.fish_prediction_percentage,
                 'tail_tip_status_list': self.tail_tip_status_list,
                 'tail_tip_found_percentage': self.tail_tip_found_percentage,
+                'swimbladder_points_status_list': self.swimbladder_points_status_list,
+                'swimbladder_found_percentage': self.swimbladder_found_percentage,
                 "paramecium": self.paramecium.export_to_struct(),
                 'is_complex_hunt': self.is_complex_hunt,
                 'outcome': self.outcome,
@@ -478,8 +516,10 @@ class Event:
                      head=Head.import_from_struct(data['head']),
                      tail=Tail.import_from_struct(data['tail']),
                      fish_tracking_status_list=data['fish_tracking_status_list'],
-                     is_head_prediction_list=data.get('is_head_prediction_list',[]),
+                     is_head_prediction_list=data.get('is_head_prediction_list', []),
+                     fish_area_in_pixels=data.get('fish_area_in_pixels', []),
                      tail_tip_status_list=data.get('tail_tip_status_list', []),
+                     swimbladder_points_status_list=data.get('swimbladder_points_status_list', []),
                      paramecium=Paramecium.import_from_struct(data['paramecium']))
         if 'is_complex_hunt' in data.keys():
             result.set_metadata(data['is_complex_hunt'], data['outcome'], data['outcome_str'], data['comments'],
@@ -493,37 +533,52 @@ class Event:
                             origin_head_points_list: np.array,
                             destination_head_points_list: np.array,
                             fish_tracking_status_list: List[bool],
+                            fish_area_in_pixels: List[float],
                             is_head_prediction_list: List[bool],
                             eyes_abs_angle_list: np.array,
                             eyes_head_dir_diff_ang_list: np.array,
                             eyes_areas_pixels_list: np.array,
                             tail_tip_point_list: np.array,
+                            swimbladder_points_list: np.array,
+                            tip_to_swimbladder_distance: List[float],
                             tail_tip_status_list: List[bool],
+                            swimbladder_points_status_list: List[bool],
                             tail_path_list: np.array,
-                            is_bout_frame_list: np.array,
+                            interpolated_tail_path: List[np.array],
+                            is_bout_frame_list: List[bool],
+                            bout_start_frames: List[int],
+                            bout_end_frames: List[int],
                             velocity_norms: np.array,
                             paramecium_tracker_output):
         """ Initialize class instance from tracker output
+
         :param eyes_areas_pixels_list:
         :param event_name: current event full name
         :param event_id: current event_id (number only)
         :param fish_tracking_status_list: list indicating if current frame data is good
+        :param fish_area_in_pixels: per frame area size (in pixels)
         :param is_head_prediction_list: list indicating if current frame head data is a prediction
         :param tail_tip_status_list: list indicating if the tail tip was found for a given frame number
+        :param swimbladder_points_status_list: list indicating if the swimbladder point was found for a given frame number
         :param destination_head_points_list: np array as returned from tracker
         :param origin_head_points_list: np array as returned from tracker
         :param eyes_abs_angle_list: nx2 float angles list (pair of eyes, per frame)
         :param eyes_head_dir_diff_ang_list: nx2 float angles list (pair of eyes, per frame)
-        :param tail_tip_point_list: nx2 integer pixel position of tail tip 
+        :param tail_tip_point_list: nx2 integer pixel position of tail tip
+        :param swimbladder_points_list: nx2 integer pixel position of swimbladder
+        :param tip_to_swimbladder_distance: n float of distance from tip of tail to swimbladder (per frame)
         :param tail_path_list: list of lists of ordered points across the fish's midline
+        :param interpolated_tail_path: np.array of shape (Frames,TailSegments,2) with points along the tail in equal distance. Tail Segments is always constant.
         :param is_bout_frame_list: list of boolean values indicating whether or not this frame is part of a bout
+        :param bout_start_frames: list containing bout start frames
+        :param bout_end_frames: list containing bout end frames
         :param velocity_norms: list of float values containing a convolution of the velocity norm, used to determine bouts
         :param paramecium_tracker_output: output data object (as defined by paramerium tracker)
         :return: class instance of one event, is instance ok (otherwise not saved)
         """
         # todo what else is mandatory for a valid mat file?
         if not Points.is_valid_points(origin_head_points_list) or \
-           not Points.is_valid_points(destination_head_points_list):
+                not Points.is_valid_points(destination_head_points_list):
             return None, False
 
         if not isinstance(paramecium_tracker_output, Paramecium):  # convert if not converted outside
@@ -537,7 +592,7 @@ class Event:
         tail_path_list_as_points = []
         for tail_path in tail_path_list:
             if type(tail_path) is not Points:
-                Points.from_array(tail_path, cast_to_float=True)
+                tail_path = Points.from_array(tail_path, cast_to_float=True)
 
             tail_path_list_as_points.append(tail_path)
         return cls(event_name=event_name,
@@ -550,13 +605,19 @@ class Event:
                              eyes_areas_pixels_list),
                    tail=Tail(Points.from_array(np.array(tail_tip_point_list)),
                              is_bout_frame_list,
+                             bout_start_frames,
+                             bout_end_frames,
                              tail_path_list_as_points,
+                             interpolated_tail_path,
+                             Points.from_array(np.array(swimbladder_points_list)),
+                             tip_to_swimbladder_distance,
                              velocity_norms),
                    fish_tracking_status_list=fish_tracking_status_list,
                    is_head_prediction_list=is_head_prediction_list,
                    tail_tip_status_list=tail_tip_status_list,
+                   fish_area_in_pixels=fish_area_in_pixels,
+                   swimbladder_points_status_list=swimbladder_points_status_list,
                    paramecium=paramecium), True
-
 class Metadata:
     """ Separate class to allow reuse.
     This data is added by the script main_metadata.py, and is read from the relevant excel file (after main.py run)
@@ -622,14 +683,17 @@ class FishPreprocessedData:
     """ Contains: Name, and events containing head direction, tail points and paramecia trajectories data (any data
     used by further processing).
     Note: This class contained inner/nested classes, since these exists within the fish's context
+
     Functionality:
     - Preprocessed from videos: initialized directly from tracker output
     - Loaded/saved from/to mat files: to be further processed in both languages.
+
     Main flow:
     - analyse by trackers (result as numpy array or classes)
     - call Event.from_tracker_output to create Event instance (FishPreprocessedData receives list of events)
     - call export_to_matlab to save the output
     -- call import_from_matlab to load the output, to be re-processed, fixed, and saved again
+
     Main functions:
     - The functions are recursively called, s.t. each class can load and save itself.
       There are 2 levels:
@@ -641,6 +705,7 @@ class FishPreprocessedData:
             These functions are called by FishPreprocessedData.
          2. from_tracker_output works against relevant tracker output class.
             Event.from_tracker_output is called by main loop, inner classes are called recursively.
+
     Note:
         - export functions are 'normal' functions.
         - import functions and from_tracker_output are classmethods, meaning they creates a class instance by calling
@@ -665,6 +730,7 @@ class FishPreprocessedData:
     def set_metadata(self, age_dpf: int, num_of_paramecia_in_plate: int, acclimation_time_min: int,
                      feeding: int, feeding_str: str):
         """Set and save additional metadata from excel file.
+
         :param age_dpf:
         :param num_of_paramecia_in_plate:
         :param acclimation_time_min:
